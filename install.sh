bc0d93048b4c4887d52ed52066ab389447950fe4
rrazban <rmr4442@gmail.com>
2017-12-20 11:38:22 -0500

    add non-python packages needed for directory

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# installing python libraries
+# installing Python libraries
 
 pip install numpy;
 pip install biopython;
 pip install bioservices;
+
+echo "also install PyMOL (to make static PDB images) and C++ (to run TMalign)"
